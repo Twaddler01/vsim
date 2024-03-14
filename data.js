e4d1bb0d8d5aec19ade698d2486899f4bd4aecd2
Twaddler01 <86488955+Twaddler01@users.noreply.github.com>
2024-03-14 01:24:28 -0500

    Update data.js

--- data.js
+++ data.js
@@ -27,8 +27,8 @@ export function init_resourcesData() {
         resourcesIndex.con_lbl = 'convert_' + resourcesIndex.id;
         resourcesIndex.con_btn = 'convert_btn_' + resourcesIndex.id;
         resourcesIndex.convert = 10;
-        resourcesIndex.cnt = 36;
-        resourcesIndex.max = 500;
+        resourcesIndex.cnt = 0;
+        resourcesIndex.max = 1000;
         //updates.print_resources = '<span class="ltbluetxt">' + resourcesIndex.lbl + ': ' + resourcesIndex.cnt + ' / ' + resourcesIndex.max + '</span>';
         resourcesIndex.res_container = 'res_container_' + resourcesIndex.id;
         resourcesIndex.res_cnt = 'res_cnt_' + resourcesIndex.id;
@@ -49,9 +49,15 @@ export function init_resourcesData() {
 export function init_upgradeData() {
 
     const upgradeData = [
-        { id: 'twigs', costs: { 'Twigs': 20, 'Pebbles': 10 }, type: 'resource' }, 
-        { id: 'pebbles', costs: { 'Twigs': 10, 'Pebbles': 20 }, type: 'resource' }, 
-        { id: 'pine_needles', costs: { 'Twigs': 10, 'Pebbles': 10, 'Pine Needles': 10 }, type: 'resource' }, 
+        { id: 'twigs', type: 'upgrade' }, 
+        { id: 'pebbles', type: 'upgrade' }, 
+        { id: 'pine_needles', type: 'upgrade' },  
+        { id: 'sticks', type: 'upgrade' }, 
+        { id: 'stones', type: 'upgrade' }, 
+        { id: 'leaves', type: 'upgrade' },  
+        { id: 'logs', type: 'upgrade' }, 
+        { id: 'rocks', type: 'upgrade' }, 
+        { id: 'brush', type: 'upgrade' }, 
     ];
     
     const resourcesData = init_resourcesData();
@@ -65,12 +71,15 @@ export function init_upgradeData() {
         if (linkedResource) {
             // Dynamically assign id from upgradeData
             // upgrade_container div
-            upgradeIndex.id = linkedResource.id + '_upgrade';
+            upgradeIndex.upgradeID = linkedResource.id + '_upgrade';
             upgradeIndex.lbl = linkedResource.lbl;
         }
         upgradeUpdates.first_line_div_id = upgradeIndex.id + '_first_line_div';
         upgradeUpdates.toggled_details_div = upgradeIndex.id + '_details';
-        upgradeUpdates.costs_div = upgradeIndex.id + '_costs_div';
+
+        // costs
+        upgradeUpdates.costs_div = 'UPGRADE_' + upgradeIndex.id.toUpperCase() + '_costs_div';
+     
         upgradeUpdates.object_count_id = upgradeIndex.id + '_object_count_id';
         upgradeUpdates.gain_id = upgradeIndex.id + '_gain';
         // [ + ] [ - ] button
@@ -84,7 +93,7 @@ export function init_upgradeData() {
         upgradeUpdates.gather_increase = 1.2;
         upgradeUpdates.cost_creep = 1.4;
         upgradeUpdates.desc = '...Use your experiences from collecting ' + upgradeIndex.lbl.toLowerCase() + ' and other things to increase the rate of collecting ' + upgradeIndex.lbl.toLowerCase() + '.';
-        upgradeUpdates.print_costs = '';
+        //upgradeUpdates.print_costs = '';
         upgradeUpdates.job = null;
         upgradeUpdates.consume = null;
         upgradeUpdates.parentID = null;
@@ -101,14 +110,16 @@ export function init_buildingData() {
 
     const buildingData = [
         { id: 'primitive_shelter_building', 
-        costs: { 'Twigs': 500, 'Pebbles': 100 }, 
+        // TEST
+        //costs: { 'Twigs': 500, 'Pebbles': 100 }, 
         title: 'Primitive Shelter',
         desc: '...A basic shelter, providing minimal protection from the elements.<br>...Provides a home for 1 tribe member.',
         gain_lbl: '+1 Tribe Member', 
         gain_detail_lbl: 'Given a job, serves as a gatherer or a hunter.'
         }, 
         { id: 'primitive_shelter_building2', 
-        costs: { 'Pebbles': 500, 'Sticks': 100 }, 
+        // TEST
+        //costs: { 'Pebbles': 500, 'Sticks': 100 }, 
         title: 'Primitive Shelter2',
         desc: '...A basic shelter, providing minimal protection from the elements.<br>...Provides a home for 1 tribe member.',
         gain_lbl: '+1 Tribe Member', 
@@ -121,15 +132,20 @@ export function init_buildingData() {
         const buildingUpdates = {};
         buildingUpdates.first_line_div_id = buildingIndex.id + '_first_line_div';
         buildingUpdates.toggled_details_div = buildingIndex.id + '_details';
-        buildingUpdates.costs_div = buildingIndex.id + '_costs_div';
-        buildingUpdates.object_count_id = buildingIndex.id + '_object_count_id';
+        buildingUpdates.object_count_id = buildingIndex.id + '_obj_cnt_id';
         buildingUpdates.gain_id = buildingIndex.id + '_gain';
         buildingUpdates.gain_detail_id = buildingIndex.id + '_gain_detail';
         buildingUpdates.button = buildingIndex.id + '_button';
         buildingUpdates.add_button = buildingIndex.id + '_add_button';
         // static
         //buildingUpdates.title = '_title_';
-        buildingUpdates.cnt = 0;
+
+        // costs
+        buildingUpdates.costs_div = buildingIndex.id + '_costs_div';
+        // costs parts
+        buildingUpdates.costs_cnt_span = buildingIndex.id + '_cnt_span';
+        buildingUpdates.costs_array_span = buildingIndex.id + '_costs_array';
+
         buildingUpdates.gather_increase = 1.2;
         buildingUpdates.cost_creep = 1.4;
         // static
@@ -139,6 +155,7 @@ export function init_buildingData() {
         buildingUpdates.parentID = null;
         buildingUpdates.maxed = false;
         buildingUpdates.ResCnt = buildingIndex.id + '_ResCnt';
+        buildingUpdates.cnt = 0;
         buildingUpdates.type = 'building';
         // Assign updates to resourceslIndex properties
         Object.assign(buildingIndex, buildingUpdates);
@@ -147,14 +164,67 @@ export function init_buildingData() {
     return buildingData;
 }
 
+export function init_jobsData() {
+    const jobsData = [
+        { id: 'gatherer', 
+        lbl: 'Gatherer', 
+        title: 'JOB: Basic Gatherer',
+        desc: '...Automatically gathers basic fruit to help feed your people.',
+        gain_lbl: '+1 Food/s', 
+        gain_detail_lbl: 'Gathers berries, wild lettuce, and mushrooms.', 
+        lvl: 1 }, 
+        { id: 'basic_hunter', 
+        lbl: 'Basic Hunter', 
+        title: 'JOB: Basic Hunter',
+        desc: '...Automatically hunts basic game to help feed your people.<br>...Requires basic weapons.',
+        gain_lbl: '+2 Food/s', 
+        gain_detail_lbl: 'Hunts squirrel and deer.', 
+        lvl: 2 }, 
+        { id: 'basic_collector', 
+        lbl: 'Basic Collector', 
+        title: 'JOB: Basic Collector',
+        desc: '...Automatically gathers resources to help supply your people with (sort of) useful materials.',
+        gain_lbl: '+1 Gather/s', 
+        gain_detail_lbl: 'Gathers these basic materials: twigs, pebbles, and pine needles.', 
+        lvl: 1 }, 
+    ];
+    
+    for (let i = 0; i < jobsData.length; i++) {
+        const jobsDataIndex = jobsData[i];
+        const jobsDataUpdates = {};
+        jobsDataUpdates.first_line_div_id = jobsDataIndex.id + '_line1_div_id';
+        jobsDataUpdates.toggled_details_div_id = jobsDataIndex.id + '_tog_div';
+        jobsDataUpdates.button_id = jobsDataIndex.id + '_button';
+        jobsDataUpdates.add_button_id = jobsDataIndex.id + '_add_button';
+        jobsDataUpdates.object_count_id = jobsDataIndex.id + '_obj_cnt_id';
+        jobsDataUpdates.maxed_display_id = jobsDataIndex.id + '_maxed_display';
+        jobsDataUpdates.gain_id = jobsDataIndex.id + '_gain_id';
+        jobsDataUpdates.gain_detail_id = jobsDataIndex.id + '_gain_detail_id';
+        // costs
+        jobsDataUpdates.costs_div = 'JOB_' + jobsDataIndex.id.toUpperCase() + '_costs_div';
+        // costs parts
+        jobsDataUpdates.costs_cnt_span = jobsDataIndex.id + '_cnt_span';
+        jobsDataUpdates.costs_array_span = jobsDataIndex.id + '_costs_array';
+
+        jobsDataUpdates.food_gain = jobsDataIndex.id + '_food_gain';
+        jobsDataUpdates.type = 'job';
+        jobsDataUpdates.cnt = 0;
+        // Assign updates to tribeData properties
+        Object.assign(jobsDataIndex, jobsDataUpdates);
+    }
+    
+    return jobsData;
+}
+
 export function init_tribeData() {
 
     const tribeData = [
-        { id: 'available_members', lbl: '-- Available Members:&nbsp;', cnt: 0, type: 'special' },
-        { id: 'tribe_leader', lbl: 'Tribe Leaders:&nbsp;', cnt: 1, type: 'special', food_gain: 0.2, food_gain_eid: 'food_gain_eid' },
-        { id: 'gatherer', lbl: 'Gatherers:&nbsp;', cnt: 0, type: 'job' },
-        { id: 'hunter', lbl: 'Hunters:&nbsp;', cnt: 0, type: 'job' },
-        { id: 'total_population', lbl: 'Total Population:&nbsp;', cnt: 0, type: 'total' },
+        { id: 'available_members', lbl: 'Available Members', print: '-- Available Members:&nbsp;', cnt: 0, type: 'job', cost_lbl: 'Available Members' },
+        { id: 'tribe_leader', lbl: 'Tribe Leaders', print: 'Tribe Leaders:&nbsp;', cnt: 1, type: 'job', food_gain: 0.2, food_gain_eid: 'food_gain_eid' },
+        { id: 'gatherer', lbl: 'Gatherers', print: 'Gatherers:&nbsp;', cnt: 0, type: 'job', cost_lbl: 'Gatherer', food_gain: 1, food_consume: 0.2 }, // net gain: 0.8
+        { id: 'basic_hunter', lbl: 'Basic Hunters', print: 'Basic Hunters:&nbsp;', cnt: 0, type: 'job', cost_lbl: 'Basic Hunter', },
+        { id: 'basic_collector', lbl: 'Basic Collectors', print: 'Basic Collectors:&nbsp;', cnt: 0, type: 'job', cost_lbl: 'Basic Collector', },
+        { id: 'total_population', lbl: 'Total Population', print: 'Total Population:&nbsp;', cnt: 0, type: 'total' },
     ];
     
     // added: eid
@@ -162,6 +232,8 @@ export function init_tribeData() {
         const tribeDataIndex = tribeData[i];
         const tribeDataUpdates = {};
         tribeDataUpdates.eid = tribeDataIndex.id + '_eid';
+        tribeDataUpdates.add_button_id = tribeDataIndex.id + '_add_button';
+        tribeDataUpdates.object_count_id = tribeDataIndex.id + '_obj_cnt_id';
         // Assign updates to tribeData properties
         Object.assign(tribeDataIndex, tribeDataUpdates);
     }
@@ -172,24 +244,25 @@ export function init_tribeData() {
 export function init_foodSources() {
 
     const foodSources = [
-        { id: 'berries', lbl: 'Berries', type: 'plant', multiplier: 1.0, cnt: 0 },
-        { id: 'mushroom', lbl: 'mushroom', type: 'plant', multiplier: 1.1, cnt: 0 },
-        { id: 'wild_lettuce', lbl: 'Wild Lettuce', type: 'plant', multiplier: 0.8, cnt: 0 },
-        { id: 'squirrel', lbl: 'Squirrel', type: 'meat', multiplier: 1.3, cnt: 0 },
-        { id: 'deer', lbl: 'Deer', type: 'meat', multiplier: 1.5, cnt: 0 },
-        { id: 'sheep', lbl: 'Sheep', type: 'meat', multiplier: 1.5, cnt: 0 },
-        { id: 'wild_boar', lbl: 'Wild Boar', type: 'meat', multiplier: 2.0, cnt: 0 },
+        { id: 'berries', lbl: 'Berries', lvl: 1, multiplier: 1.0, cnt: 0 },
+        { id: 'mushroom', lbl: 'mushroom', lvl: 1, multiplier: 1.1, cnt: 0 },
+        { id: 'wild_lettuce', lbl: 'Wild Lettuce', lvl: 1, multiplier: 0.8, cnt: 0 },
+        { id: 'squirrel', lbl: 'Squirrel', lvl: 2, multiplier: 1.3, cnt: 0 },
+        { id: 'deer', lbl: 'Deer', lvl: 2, multiplier: 1.5, cnt: 0 },
+        { id: 'sheep', lbl: 'Sheep', lvl: 3, multiplier: 1.8, cnt: 0 },
+        { id: 'wild_boar', lbl: 'Wild Boar', lvl: 3, multiplier: 2.0, cnt: 0 },
     ];
     
     // added: spoil
     for (let i = 0; i < foodSources.length; i++) {
         const foodSourcesIndex = foodSources[i];
         const foodSourcesUpdates = {};
-        if (foodSourcesIndex.type === 'plant') {
-            foodSourcesUpdates.spoil = -0.2;
-        }
-        if (foodSourcesIndex.type === 'meat') {
+        foodSourcesUpdates.spoil = 0
+        if (foodSourcesIndex.lvl === 1) {
             foodSourcesUpdates.spoil = -0.4;
+        }
+        if (foodSourcesIndex.lvl === 2 || foodSourcesIndex.lvl === 3) {
+            foodSourcesUpdates.spoil = -0.2;
         }
         //foodSourcesUpdates.000 = ???
         // Assign updates to resourceslIndex properties
@@ -231,11 +304,90 @@ export function init_objectiveData() {
 
 }
 
+// migrate ALL costs
+// ids only
+export function init_costsData() {
+
+    // matches ids only
+    const costsData = [
+        // upgradeData
+        { id: 'twigs', costs: { 'twigs': 20, 'pebbles': 10 }, type: 'upgrade' }, 
+        { id: 'pebbles', costs: { 'twigs': 10, 'pebbles': 20 }, type: 'upgrade' }, 
+        { id: 'pine_needles', costs: { 'twigs': 10, 'pebbles': 10, 'pine_needles': 10 }, type: 'upgrade' },  
+        { id: 'sticks', costs: { 'sticks': 20, 'stones': 10 }, type: 'upgrade' }, 
+        { id: 'stones', costs: { 'sticks': 10, 'stones': 20 }, type: 'upgrade' }, 
+        { id: 'leaves', costs: { 'sticks': 10, 'stones': 10, 'leaves': 10 }, type: 'upgrade' },  
+        { id: 'logs', costs: { 'logs': 20, 'rocks': 10 }, type: 'upgrade' }, 
+        { id: 'rocks', costs: { 'rocks': 20, 'logs': 10 }, type: 'upgrade' }, 
+        { id: 'brush', costs: { 'logs': 10, 'rocks': 10, 'brush': 10 }, type: 'upgrade' }, 
+        // buildingData
+        { id: 'primitive_shelter_building', costs: { 'twigs': 500, 'pebbles': 100 }, type: 'building' }, 
+        { id: 'primitive_shelter_building2', costs: { 'pebbles': 500, 'sticks': 100 }, type: 'building' }, 
+        // jobsData
+        { id: 'gatherer', costs: { 'available_members': 1 }, type: 'job', }, 
+        { id: 'basic_hunter', costs: { 'available_members': 1 }, type: 'job' }, 
+        { id: 'basic_collector', costs: { 'available_members': 1 }, type: 'job' }, 
+    ];
+
+    return costsData;
+}
+
+export function init_objectElements() {
+    
+    // NOTES
+    // .eid = created element (.id)
+    // (for first 3) .parent = objectType + ___ 
+
+    // temporary to skip errors
+    let object = [ { cnt: null, type: 'none' } ];
+    
+    const objectElements = [
+        { id: '_section', element: 'div', parentID: '_sect_id', style: null, className: null, inner: null }, 
+        { id: '_container', element: 'div', parentID: '_section', style: null, className: null, inner: null }, 
+        { id: 'first_line_div', element: 'div', parentID: '_container', style: null, className: null, inner: null }, 
+        { id: 'toggled_details_div', element: 'div', parentID: '_container', style: 'no_display', className: null, inner: null }, 
+        { id: 'button', element: 'span', parentID: 'first_line_div', style: null, className: null, inner: `<hr class="divider" width=30% align="left"><span id="${object.button}">[ + ] <span class="button_orange">${object.title}</span></span> ` }, 
+        // BUTTON ACTION HERE 'toggleButton'
+        { id: 'object_count', element: 'span', parentID: 'first_line_div', style: null, className: 'button_orange', inner: `${object.cnt} ` }, 
+        // HIDE IF = 1
+        { id: 'add_button', element: 'span', parentID: 'first_line_div', style: null, className: 'ltred', inner: `${object.type}.toUpperCase()` }, // '[ TYPE ]'
+        { id: 'add_button_maxed_display', element: 'span', parentID: 'first_line_div', style: null, className: null, inner: null }, 
+        { id: 'gain_lbl', element: 'div', parentID: 'toggled_details_div', style: null, className: 'ltgreentxt', inner: '+20% Gather Rate' }, 
+        { id: 'gain_detail', element: 'div', parentID: 'toggled_details_div', style: null, className: 'light_small', inner: `Next: +${object.gather_increase} ${object.lbl}.toLowerCase()` }, 
+        { id: 'description', element: 'div', parentID: 'toggled_details_div', style: 'maxWidth60', className: null, inner: `${object.desc}` }, 
+        { id: 'costs_lbl', element: 'div', parentID: 'toggled_details_div', style: null, className: null, inner: `<p class="yellowtxt">COSTS:</p>` }, 
+        { id: 'costs_div', element: 'div', parentID: 'toggled_details_div', style: null, className: null, inner: null }, 
+        // WIP extras
+        //{ id: 'job_div', element: 'div', parent: 'toggled_details_div', style: 'no_display', className: null, inner: `<p class="yellowtxt">CIVILIAN JOB:</p>` }, 
+        //{ id: 'consume_div', element: 'div', parent: 'toggled_details_div', style: 'no_display', className: null, inner: `<p class="yellowtxt">CONSUMES:</p>` }, 
+    ];
+
+/*
+    // assign in main file
+    let objects = 'objectName';
+
+    objectElements.forEach(element => {
+        if (element.id.startsWith('_')) {
+            element.id = objects + element.id;
+        }
+        if (element.parentID.startsWith('_')) {
+            element.parentID = objects + element.parentID;
+        }
+        console.log(element);
+    });
+*/
+
+    return objectElements;
+}
+
 export const resourcesData = init_resourcesData();
 export const upgradeData = init_upgradeData();
 export const buildingData = init_buildingData();
+export const jobsData = init_jobsData();
 export const tribeData = init_tribeData();
 export const foodSources = init_foodSources();
 export const foodResource = init_foodResource();
 export const goalsData = init_goalsData();
 export const objectiveData = init_objectiveData();
+export const costsData = init_costsData();
+export const objectElements = init_objectElements();
